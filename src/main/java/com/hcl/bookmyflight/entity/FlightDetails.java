734b2ddb1cbc112a67769035f72d4732e374b0f0
karanrathod123 <karanrathod2256@gmail.com>
2019-07-18 10:28:54 +0530

    FlightDetail commit

---
+++ src/main/java/com/hcl/bookmyflight/entity/FlightDetails.java
@@ -0,0 +1,57 @@
+package com.hcl.bookmyflight.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class FlightDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO )
+	private Long flightId;
+	private String source;
+	private String destination;
+	private LocalDateTime dateTime;
+	private double price;
+	
+	public Long getFlightId() {
+		return flightId;
+	}
+	public void setFlightId(Long flightId) {
+		this.flightId = flightId;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+}
+
