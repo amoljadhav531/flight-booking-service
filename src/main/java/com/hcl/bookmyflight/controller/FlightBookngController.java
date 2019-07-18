c5e63f6583681603478d1860c317370ef31c62e9
piyush <piyushr999@gmail.com>
2019-07-18 11:49:35 +0530

    change bookingdetailscontroller logic

--- src/main/java/com/hcl/bookmyflight/controller/FlightBookngController.java
+++ src/main/java/com/hcl/bookmyflight/controller/FlightBookngController.java
@@ -2,17 +2,20 @@ package com.hcl.bookmyflight.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.bookmyflight.dto.BookingDetailsDto;
 
 /**
  * @author Administrator
  *
  */
 @RestController
-public class FlightBooingController {
+public class FlightBookngController {
 
 	@PostMapping("/book")
-	public ResponseEntity<?> bookFlight() {
+	public ResponseEntity<?> bookFlight(@RequestBody BookingDetailsDto bookingDetailsDto) {
 		return null;
 	}
 }
