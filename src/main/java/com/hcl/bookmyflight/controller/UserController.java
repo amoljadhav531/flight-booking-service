6d4c7fc6e724f406ae6f70980542ec74af3797aa
prajaktajoshi123 <joshiprajakta12493@gmail.com>
2019-07-18 11:00:58 +0530

    Added User Controller

---
+++ src/main/java/com/hcl/bookmyflight/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.hcl.bookmyflight.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+
+import com.hcl.bookmyflight.entity.User;
+
+public class UserController {
+
+	@PostMapping("/login")
+	public ResponseEntity<User> LoginUser(@RequestAttribute String username ,@RequestAttribute String password)
+	{
+		
+		return ResponseEntity.ok(null);
+	}
+}
