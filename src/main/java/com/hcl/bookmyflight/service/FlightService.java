60a1984134d725c24aa9f797c0f123ea6952fe05
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-19 13:35:15 +0530

    Added changes for permission API

--- src/main/java/com/hcl/bookmyflight/service/FlightService.java
+++ src/main/java/com/hcl/bookmyflight/service/FlightService.java
@@ -1,5 +1,10 @@
 package com.hcl.bookmyflight.service;
 
+import com.hcl.bookmyflight.dto.FlightPermission;
+import com.hcl.bookmyflight.dto.ResponseData;
+
 public interface FlightService {
+	
+	public ResponseData grantFlightPermissions(FlightPermission permission) ;
 
 }
