ae6618a8b0e626e1cc850b4efc9b2ffca57b7592
Harsimar <simarkaur1295@gmail.com>
2019-07-19 15:40:07 +0530

    implemented lombok

--- src/main/java/com/hcl/bookmyflight/dto/ResponseData.java
+++ src/main/java/com/hcl/bookmyflight/dto/ResponseData.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseData implements Serializable {
 
 	private static final long serialVersionUID = 4806995052658367581L;
