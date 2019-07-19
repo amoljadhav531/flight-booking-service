0ec331cfda95c345f3aec86740aef12adcd5545b
Harsimar <simarkaur1295@gmail.com>
2019-07-19 12:27:30 +0530

    Day2 git pull

--- src/main/java/com/hcl/bookmyflight/controller/SearchFlightController.java
+++ src/main/java/com/hcl/bookmyflight/controller/SearchFlightController.java
@@ -1,16 +1,17 @@
 package com.hcl.bookmyflight.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hcl.bookmyflight.dto.FlightDetailsDTO;
 import com.hcl.bookmyflight.entity.FlightDetails;
 import com.hcl.bookmyflight.service.SearchFlightService;
 
@@ -22,9 +23,10 @@ public class SearchFlightController {
 	@Autowired
 	private SearchFlightService searchFlightService;
 	
-	@PostMapping("")
-	public ResponseEntity<List<FlightDetails>> searchFlight(@RequestBody FlightDetailsDTO searchFlightDTO) {
-		return new ResponseEntity<>(searchFlightService.searchFlight(searchFlightDTO) ,HttpStatus.OK);
+	@GetMapping()
+	public ResponseEntity<List<FlightDetails>> searchFlight(@RequestParam String source, @RequestParam String destination, @RequestParam String sortparam, @RequestParam String date) {
+		LocalDate localDate = LocalDate.parse(date);
+		return new ResponseEntity<>(searchFlightService.searchFlight(source,destination, sortparam, localDate) ,HttpStatus.OK);
 	}
 	
 }
--- src/main/java/com/hcl/bookmyflight/service/SearchFlightService.java
+++ src/main/java/com/hcl/bookmyflight/service/SearchFlightService.java
@@ -1,5 +1,7 @@
 package com.hcl.bookmyflight.service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +18,36 @@ public class SearchFlightService {
 	@Autowired
 	private FlightDetailsRepository searchFlightRepository;
 	 	
-	public List<FlightDetails> searchFlight(FlightDetailsDTO searchFlightDTO) {
-		
-		if(searchFlightDTO.getSortParam().equalsIgnoreCase("price")) {
-			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByPriceAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
-		}	
-		else if(searchFlightDTO.getSortParam().equalsIgnoreCase("timeDuration")) {
-			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByTimeDurationAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
+//	public List<FlightDetails> searchFlight(FlightDetailsDTO searchFlightDTO) {
+//		
+//		if(searchFlightDTO.getSortParam().equalsIgnoreCase("price")) {
+//			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByPriceAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
+//		}	
+//		else if(searchFlightDTO.getSortParam().equalsIgnoreCase("timeDuration")) {
+//			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByTimeDurationAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
+//		}
+//		else if(searchFlightDTO.getSortParam().equalsIgnoreCase("arrivalTime")) {
+//			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByArrivalTimeAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
+//		}
+//		else {
+//			throw new ResourceNotFoundException("Sort Parameter is not specified properly. Please specify out of these <price, timeDuration, arrivalTime>", "", searchFlightDTO.getSortParam());
+//		}
+//		
+//	}
+
+	public List<FlightDetails> searchFlight(String source, String destination, String sortparam, LocalDate date) {
+		if(sortparam.equalsIgnoreCase("price")) {
+			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByPriceAsc(source, destination, date);
 		}
-		else if(searchFlightDTO.getSortParam().equalsIgnoreCase("arrivalTime")) {
-			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByArrivalTimeAsc(searchFlightDTO.getSource(), searchFlightDTO.getDestination(), searchFlightDTO.getDate());
+		else if(sortparam.equalsIgnoreCase("timeDuration")) {
+			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByTimeDurationAsc(source, destination, date);
+		}
+		else if(sortparam.equalsIgnoreCase("arrivalTime")) {
+			return searchFlightRepository.findBySourceAndDestinationAndDateOrderByArrivalTimeAsc(source, destination, date);
 		}
 		else {
-			throw new ResourceNotFoundException("Sort Parameter is not specified properly. Please specify out of these <price, timeDuration, arrivalTime>", "", searchFlightDTO.getSortParam());
+			throw new ResourceNotFoundException("Sort Parameter is not specified properly. Please specify out of these <price, timeDuration, arrivalTime>", "", sortparam);
 		}
-		
 	}
 
 }
